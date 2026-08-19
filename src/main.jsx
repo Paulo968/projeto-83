ff3a23dedece15fae380bb8e3963516fa8b51f70
Paulo Zaqueu <paulozaqueu3@gmail.com>
2026-08-19 14:44:52 -0300

    feat: ativa experiência mobile V2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './AppV2.jsx'
 import './styles.css'
+import './styles-video.css'
+import './styles-mobile-v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
