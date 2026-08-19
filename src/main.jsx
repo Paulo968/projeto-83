2dd283761f7945677946a03113a0479de8b540f4
Paulo Zaqueu <paulozaqueu3@gmail.com>
2026-08-19 15:48:22 -0300

    style: carrega melhorias da ficha de treino v3

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles.css'
 import './styles-video.css'
 import './styles-mobile-v2.css'
 import './styles-exercise-detail.css'
+import './styles-training-v3.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
