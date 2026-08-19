ac4a557df181d1e9e267ce83d060d063120db3ef
Paulo Zaqueu <paulozaqueu3@gmail.com>
2026-08-19 15:00:21 -0300

    feat: ativa tela completa de cada exercício

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppV2.jsx'
+import App from './AppV3.jsx'
 import './styles.css'
 import './styles-video.css'
 import './styles-mobile-v2.css'
+import './styles-exercise-detail.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
