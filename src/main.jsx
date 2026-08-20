3b0006cf7c377805eb1a41472d312d9d6b118b9c
Paulo Zaqueu <paulozaqueu3@gmail.com>
2026-08-19 22:54:27 -0300

    feat: enable Projeto 83 as installable PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './AppV3.jsx'
+import { setupPWA } from './pwa.js'
 import './styles.css'
 import './styles-video.css'
 import './styles-mobile-v2.css'
 import './styles-exercise-detail.css'
 import './styles-training-v3.css'
+
+setupPWA()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
